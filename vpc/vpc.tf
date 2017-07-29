c8acc9f755976d3f74a6d10769c323fa15cc7228
Raj Gajjelli <gajjelli.r@gmail.com>
2017-07-29 10:04:22 +0100

    Updated for dns support -5.0

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -11,8 +11,8 @@ variable "allow_all_cidr" { }
 # vpc
 resource "aws_vpc" "main" {
   cidr_block = "${var.vpc_cidr}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
   tags {
     Name = "${var.env}-vpc"
   }
